9a01dcf9553fb6a213f9d83335f4c4383117f5ca
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-07-25 22:23:37 -0400

    Basic foundation improvements

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    fontawesome: {
+      icons: {
+        'free-solid-svg-icons': [
+          'balance-scale',
+        ],
+      },
+    },
   });
 
   app.import('node_modules/foundation-sites/dist/css/foundation.css');

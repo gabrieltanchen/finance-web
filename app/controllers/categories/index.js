e87b50f17a93b368837c6e31c54f9d2aedb9f5c1
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-07-27 06:13:06 -0400

    Create category form

--- app/controllers/categories/index.js
+++ app/controllers/categories/index.js
@@ -1,13 +1,18 @@
 import { alias } from '@ember/object/computed';
 import Controller from '@ember/controller';
 import { set } from '@ember/object';
+import CategoryValidations from '../../validations/category';
 
 export default Controller.extend({
   categories: alias('model.categories'),
+  CategoryValidations,
   newCategory: alias('model.newCategory'),
   queryParams: ['create', 'limit', 'page'],
 
   actions: {
+    closeCreateForm() {
+      set(this, 'create', null);
+    },
     showCreateForm() {
       set(this, 'create', true);
     },
---
+++ app/validations/category.js
@@ -0,0 +1,7 @@
+import {
+  validatePresence,
+} from 'ember-changeset-validations/validators';
+
+export default {
+  name: validatePresence(true),
+};

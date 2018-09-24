c244928eb0ccc48edd031f51e4b005c9ef38bf28
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-09-23 21:11:22 -0400

    templates

--- app/controllers/categories/subcategories.js
+++ app/controllers/categories/subcategories.js
@@ -1,4 +1,6 @@
+import { alias } from '@ember/object/computed';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  category: alias('model.category'),
 });
--- app/routes/categories/expenses.js
+++ app/routes/categories/expenses.js
@@ -1,4 +1,20 @@
 import Route from '@ember/routing/route';
+import { get } from '@ember/object';
+import RSVP from 'rsvp';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  session: service(),
+
+  async beforeModel() {
+    if (!(await get(this, 'session').isLoggedIn())) {
+      this.transitionTo('login');
+    }
+  },
+
+  model(params) {
+    return RSVP.hash({
+      category: this.get('store').findRecord('category', params.category_uuid),
+    });
+  },
 });

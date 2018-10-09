3868579016bbea1ec50b96e69f12d65c3b5c69ba
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-10-09 00:16:48 -0400

    show routes

--- app/routes/vendors/expenses.js
+++ app/routes/vendors/expenses.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { get } from '@ember/object';
+import RSVP from 'rsvp';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
@@ -9,5 +10,11 @@ export default Route.extend({
     if (!(await get(this, 'session').isLoggedIn())) {
       this.transitionTo('login');
     }
+  },
+
+  model(params) {
+    return RSVP.hash({
+      vendor: get(this, 'store').findRecord('vendor', params.vendor_uuid),
+    });
   },
 });

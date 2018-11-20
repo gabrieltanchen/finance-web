70bcdc18b0aa23856c0d00e22ed8b038f4cb12c4
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-11-20 17:35:33 -0500

    log-out-redirect-test

--- tests/acceptance/log-out-redirect-test.js
+++ tests/acceptance/log-out-redirect-test.js
@@ -1,34 +1,26 @@
-// import {
-//   describe,
-//   it,
-//   beforeEach,
-//   afterEach,
-// } from 'mocha';
-// import { expect } from 'chai';
-// import startApp from 'finance-web/tests/helpers/start-app';
-// import destroyApp from 'finance-web/tests/helpers/destroy-app';
-// import { get } from '@ember/object';
-//
-// describe('Acceptance | log out redirect', function() {
-//   let application;
-//
-//   beforeEach(function() {
-//     application = startApp();
-//     const container = application.__container__;
-//     const session = container.lookup('service:session');
-//     session.logout();
-//     get(session, 'cookie').setCookie('token', 'token');
-//   });
-//
-//   afterEach(function() {
-//     destroyApp(application);
-//   });
-//
-//   it('should redirect when logging out', function() {
-//     visit('/dashboard');
-//     click('#menu-logout');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/');
-//     });
-//   });
-// });
+import { module, test } from 'qunit';
+import {
+  click,
+  currentURL,
+  visit,
+} from '@ember/test-helpers';
+import { get } from '@ember/object';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | log out redirect', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(function() {
+    const session = this.owner.lookup('service:session');
+    session.logout();
+    get(session, 'cookie').setCookie('token', 'token');
+  });
+
+  test('should redirect when logging out', async function(assert) {
+    await visit('/dashboard');
+    await click('#menu-logout');
+    assert.equal(currentURL(), '/');
+  });
+});

7d7172be848808d1668358855d32ea3b56e64829
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-11-20 17:53:10 -0500

    logged-in-redirects-test

--- tests/acceptance/logged-in-redirects-test.js
+++ tests/acceptance/logged-in-redirects-test.js
@@ -1,103 +1,74 @@
-// import {
-//   describe,
-//   it,
-//   beforeEach,
-//   afterEach,
-// } from 'mocha';
-// import { expect } from 'chai';
-// import startApp from 'finance-web/tests/helpers/start-app';
-// import destroyApp from 'finance-web/tests/helpers/destroy-app';
-// import { get } from '@ember/object';
-//
-// describe('Acceptance | logged in redirects', function() {
-//   let application;
-//
-//   beforeEach(function() {
-//     application = startApp();
-//     const container = application.__container__;
-//     const session = container.lookup('service:session');
-//     session.logout();
-//     get(session, 'cookie').setCookie('token', 'token');
-//   });
-//
-//   afterEach(function() {
-//     destroyApp(application);
-//   });
-//
-//   it('should redirect away from /', function() {
-//     visit('/');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/dashboard');
-//     });
-//   });
-//
-//   it('can visit /categories', function() {
-//     visit('/categories');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/categories');
-//     });
-//   });
-//
-//   it('can visit /categories/:uuid', function() {
-//     visit('/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee');
-//     });
-//   });
-//
-//   it('can visit /categories/:uuid/expenses', function() {
-//     visit('/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee/expenses');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee/expenses');
-//     });
-//   });
-//
-//   it('can visit /categories/:uuid/subcategories', function() {
-//     visit('/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee/subcategories');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee/subcategories');
-//     });
-//   });
-//
-//   it('can visit /dashboard', function() {
-//     visit('/dashboard');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/dashboard');
-//     });
-//   });
-//
-//   it('should redirect away from /login', function() {
-//     visit('/login');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/dashboard');
-//     });
-//   });
-//
-//   it('should redirect away from /sign-up', function() {
-//     visit('/sign-up');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/dashboard');
-//     });
-//   });
-//
-//   it('can visit /vendors', function() {
-//     visit('/vendors');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/vendors');
-//     });
-//   });
-//
-//   it('can visit /vendors/:uuid', function() {
-//     visit('/vendors/b6f0441e-bdee-4172-a646-4d8c9191db57');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/vendors/b6f0441e-bdee-4172-a646-4d8c9191db57');
-//     });
-//   });
-//
-//   it('can visit /vendors/:uuid/expenses', function() {
-//     visit('/vendors/b6f0441e-bdee-4172-a646-4d8c9191db57/expenses');
-//     return andThen(() => {
-//       expect(currentURL()).to.equal('/vendors/b6f0441e-bdee-4172-a646-4d8c9191db57/expenses');
-//     });
-//   });
-// });
+import { module, test } from 'qunit';
+import {
+  currentURL,
+  visit,
+} from '@ember/test-helpers';
+import { get } from '@ember/object';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | logged in redirects', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(function() {
+    const session = this.owner.lookup('service:session');
+    session.logout();
+    get(session, 'cookie').setCookie('token', 'token');
+  });
+
+  test('should redirect away from /', async function(assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/dashboard');
+  });
+
+  test('can visit /categories', async function(assert) {
+    await visit('/categories');
+    assert.equal(currentURL(), '/categories');
+  });
+
+  test('can visit /categories/:uuid', async function(assert) {
+    await visit('/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee');
+    assert.equal(currentURL(), '/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee');
+  });
+
+  test('can visit /categories/:uuid/expenses', async function(assert) {
+    await visit('/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee/expenses');
+    assert.equal(currentURL(), '/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee/expenses');
+  });
+
+  test('can visit /categories/:uuid/subcategories', async function(assert) {
+    await visit('/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee/subcategories');
+    assert.equal(currentURL(), '/categories/14aa3ef4-193f-45c4-8e33-ad7b79a3e6ee/subcategories');
+  });
+
+  test('can visit /dashboard', async function(assert) {
+    await visit('/dashboard');
+    assert.equal(currentURL(), '/dashboard');
+  });
+
+  test('should redirect away from /login', async function(assert) {
+    await visit('/login');
+    assert.equal(currentURL(), '/dashboard');
+  });
+
+  test('should redirect away from /sign-up', async function(assert) {
+    await visit('/sign-up');
+    assert.equal(currentURL(), '/dashboard');
+  });
+
+  test('can visit /vendors', async function(assert) {
+    await visit('/vendors');
+    assert.equal(currentURL(), '/vendors');
+  });
+
+  test('can visit /vendors/:uuid', async function(assert) {
+    await visit('/vendors/b6f0441e-bdee-4172-a646-4d8c9191db57');
+    assert.equal(currentURL(), '/vendors/b6f0441e-bdee-4172-a646-4d8c9191db57');
+  });
+
+  test('can visit /vendors/:uuid/expenses', async function(assert) {
+    await visit('/vendors/b6f0441e-bdee-4172-a646-4d8c9191db57/expenses');
+    assert.equal(currentURL(), '/vendors/b6f0441e-bdee-4172-a646-4d8c9191db57/expenses');
+  });
+});

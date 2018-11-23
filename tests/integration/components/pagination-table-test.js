b4dcb7fafdf7631e2a4829d1f5b42f86198f4852
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2018-11-22 19:06:04 -0500

    pagination-table tests

--- tests/integration/components/pagination-table-test.js
+++ tests/integration/components/pagination-table-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | pagination-table', function(hooks) {
@@ -24,6 +24,75 @@ module('Integration | Component | pagination-table', function(hooks) {
     await render(hbs`{{pagination-table meta=meta}}`);
 
     assert.ok(this.element.querySelector('.pagination-previous').classList.contains('disabled'));
+    assert.notOk(this.element.querySelector('.pagination-previous a'));
     assert.ok(this.element.querySelector('.pagination-next').classList.contains('disabled'));
+    assert.notOk(this.element.querySelector('.pagination-next a'));
+  });
+
+  test('should disable previous when on first page', async function(assert) {
+    this.set('meta', {
+      pages: 5,
+    });
+    this.set('page', 1);
+
+    await render(hbs`{{pagination-table meta=meta page=page}}`);
+
+    assert.ok(this.element.querySelector('.pagination-previous').classList.contains('disabled'));
+    assert.notOk(this.element.querySelector('.pagination-previous a'));
+    assert.notOk(this.element.querySelector('.pagination-next').classList.contains('disabled'));
+    assert.ok(this.element.querySelector('.pagination-next a'));
+  });
+
+  test('should disable next when on last page', async function(assert) {
+    this.set('meta', {
+      pages: 5,
+    });
+    this.set('page', 5);
+
+    await render(hbs`{{pagination-table meta=meta page=page}}`);
+
+    assert.notOk(this.element.querySelector('.pagination-previous').classList.contains('disabled'));
+    assert.ok(this.element.querySelector('.pagination-previous a'));
+    assert.ok(this.element.querySelector('.pagination-next').classList.contains('disabled'));
+    assert.notOk(this.element.querySelector('.pagination-next a'));
+  });
+
+  test('should go to previous page', async function(assert) {
+    this.set('meta', {
+      pages: 5,
+    });
+    this.set('page', 3);
+
+    await render(hbs`{{pagination-table meta=meta page=page}}`);
+
+    await click('.pagination-previous a');
+
+    assert.equal(this.get('page'), 2);
+  });
+
+  test('should go to next page', async function(assert) {
+    this.set('meta', {
+      pages: 5,
+    });
+    this.set('page', 3);
+
+    await render(hbs`{{pagination-table meta=meta page=page}}`);
+
+    await click('.pagination-next a');
+
+    assert.equal(this.get('page'), 4);
+  });
+
+  test('should jump to page', async function(assert) {
+    this.set('meta', {
+      pages: 5,
+    });
+    this.set('page', 3);
+
+    await render(hbs`{{pagination-table meta=meta page=page}}`);
+
+    await click('.page-link a');
+
+    assert.equal(this.get('page'), 1);
   });
 });

8c6f4194dbd64492c9a1010e0a01a96b49966280
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-11-14 20:09:26 -0500

    household members expenses

--- app/router.js
+++ app/router.js
@@ -20,6 +20,9 @@ Router.map(function() {
   });
   this.route('dashboard');
   this.route('household-members', function() {
+    this.route('expenses', {
+      path: '/:member_uuid/expenses',
+    });
     this.route('show', {
       path: '/:member_uuid',
     });

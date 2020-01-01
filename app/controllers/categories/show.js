98d6f070ecc058c795bbf8e1afc84f8f16313306
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-12-31 20:50:01 -0500

    delete categories

--- app/controllers/categories/show.js
+++ app/controllers/categories/show.js
@@ -1,6 +1,26 @@
-import { alias } from '@ember/object/computed';
 import Controller from '@ember/controller';
+import { get, set } from '@ember/object';
+import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
   category: alias('model.category'),
+  errors: null,
+
+  actions: {
+    async deleteCategory() {
+      const category = get(this, 'category');
+      try {
+        await category.destroyRecord();
+        this.transitionToRoute('categories.index');
+      } catch (err) {
+        let errors = ['Unable to delete category.'];
+        if (err && err.errors) {
+          errors = err.errors.map((error) => {
+            return error.detail;
+          });
+        }
+        set(this, 'errors', errors);
+      }
+    },
+  },
 });

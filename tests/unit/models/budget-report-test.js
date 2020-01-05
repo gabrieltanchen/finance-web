e17c8f5af4378e9fe39db4fd61c1ad7499ed2b10
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-01-04 23:25:17 -0500

    add model tests

--- tests/unit/models/budget-report-test.js
+++ tests/unit/models/budget-report-test.js
@@ -1,13 +1,40 @@
-import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { get, set } from '@ember/object';
+import { module, test } from 'qunit';
 
 module('Unit | Model | budget report', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
   test('it exists', function(assert) {
     const store = this.owner.lookup('service:store');
     const model = store.createRecord('budget-report', {});
     assert.ok(model);
+  });
+
+  test('should have alertState=false when actual_cents < budget_cents', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('budget-report', {});
+    set(model, 'actual_cents', 1);
+    set(model, 'budget_cents', 2);
+
+    assert.equal(get(model, 'alertState'), false);
+  });
+
+  test('should have alertState=false when actual_cents == budget_cents', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('budget-report', {});
+    set(model, 'actual_cents', 1);
+    set(model, 'budget_cents', 1);
+
+    assert.equal(get(model, 'alertState'), false);
+  });
+
+  test('should have alertState=true when actual_cents > budget_cents', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('budget-report', {});
+    set(model, 'actual_cents', 2);
+    set(model, 'budget_cents', 1);
+
+    assert.equal(get(model, 'alertState'), true);
   });
 });

30dbf3187648db2994fb32bbd661724516f8dc7b
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-02-29 19:57:59 -0500

    some tests

--- tests/unit/models/monthly-report-test.js
+++ tests/unit/models/monthly-report-test.js
@@ -1,5 +1,6 @@
-import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { get, set } from '@ember/object';
+import { module, test } from 'qunit';
 
 module('Unit | Model | monthly report', function(hooks) {
   setupTest(hooks);
@@ -10,21 +11,74 @@ module('Unit | Model | monthly report', function(hooks) {
     assert.ok(model);
   });
 
-  test('should have correct actual_str when actual_cents=0');
+  test('should have correct actual_str when actual_cents=0', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('monthly-report', {});
+    set(model, 'actual_cents', 0);
 
-  test('should have correct actual_str when actual_cents=500');
+    assert.equal(get(model, 'actual_str'), '-');
+  });
 
-  test('should have correct actual_str when actual_cents=12345');
+  test('should have correct actual_str when actual_cents=500', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('monthly-report', {});
+    set(model, 'actual_cents', 500);
 
-  test('should have budget_actual_diff_alert=false when actual_cents < budget_cents');
+    assert.equal(get(model, 'actual_str'), '$5.00');
+  });
 
-  test('should have budget_actual_diff_alert=false when actual_cents = budget_cents');
+  test('should have correct actual_str when actual_cents=12345', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('monthly-report', {});
+    set(model, 'actual_cents', 12345);
 
-  test('should have budget_actual_diff_alert=true when actual_cents > budget_cents');
+    assert.equal(get(model, 'actual-str'), '$123.45');
+  });
 
-  test('should have correct budget_actual_diff_str when actual_cents=0 and budget_cents=0');
+  test('should have budget_actual_diff_alert=false when actual_cents < budget_cents', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('monthly-report', {});
+    set(model, 'actual_cents', 1);
+    set(model, 'budget_cents', 2);
 
-  test('should have correct budget_actual_diff_str when actual_cents = 500 and budget_cents=1000');
+    assert.equal(get(model, 'budget_actual_diff_alert'), false);
+  });
+
+  test('should have budget_actual_diff_alert=false when actual_cents = budget_cents', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('monthly-report', {});
+    set(model, 'actual_cents', 1);
+    set(model, 'budget_cents', 1);
+
+    assert.equal(get(model, 'budget_actual_diff_alert'), false);
+  });
+
+  test('should have budget_actual_diff_alert=true when actual_cents > budget_cents', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('monthly-report', {});
+    set(model, 'actual_cents', 2);
+    set(model, 'budget_cents', 1);
+
+    assert.equal(get(model, 'budget_actual_diff_alert'), true);
+  });
+
+  test('should have correct budget_actual_diff_str when actual_cents=0 and budget_cents=0', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('monthly-report', {});
+    set(model, 'actual_cents', 0);
+    set(model, 'budget_cents', 0);
+
+    assert.equal(get(model, 'budget_actual_diff_str'), '-');
+  });
+
+  test('should have correct budget_actual_diff_str when actual_cents = 500 and budget_cents=1000', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const model = store.createRecord('monthly-report', {});
+    set(model, 'actual_cents', 500);
+    set(model, 'budget_cents', 1000);
+
+    assert.equal(get(model, 'budget_actual_diff_str'), '$5.00');
+  });
 
   test('should have correct budget_actual_diff_str when actual_cents=1000 and budget_cents=500');
 

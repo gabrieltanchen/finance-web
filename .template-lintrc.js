1df328d81b8e98943a8945fc34083bf611c623c4
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-09-08 07:49:41 -0400

    fix linter errors

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: 'octane',
   rules: {
     'no-action': false,
-    'no-bare-strings': true,
+    'no-bare-strings': false,
     'no-inline-styles': false,
     'no-invalid-interactive': false,
     'no-invalid-meta': false,

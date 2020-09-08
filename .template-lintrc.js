39b3303179ecf473d629f54f2f042d4872100525
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2020-09-08 07:32:05 -0400

    add lintrc

---
+++ .template-lintrc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  extends: 'octane',
+  rules: {
+    'no-action': false,
+    'no-bare-strings': true,
+    'no-inline-styles': false,
+    'no-invalid-interactive': false,
+    'no-invalid-meta': false,
+    'no-triple-curlies': false,
+  },
+};

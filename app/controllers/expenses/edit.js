8ac6fda1a069296ba008a2e39869af05d347db7f
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-08-12 22:52:40 -0400

    Add funds query to expense edit page

--- app/controllers/expenses/edit.js
+++ app/controllers/expenses/edit.js
@@ -4,6 +4,7 @@ import { alias } from '@ember/object/computed';
 
 export default class ExpensesEditController extends Controller {
   @alias('model.expense') expense;
+  @alias('model.funds') funds;
   @alias('model.householdMembers') householdMembers;
   @alias('model.subcategories') subcategories;
   @alias('model.vendors') vendors;
--- app/routes/expenses/edit.js
+++ app/routes/expenses/edit.js
@@ -14,6 +14,7 @@ export default class ExpensesEditRoute extends Route {
   model(params) {
     return RSVP.hash({
       expense: this.store.findRecord('expense', params.expense_id),
+      funds: this.store.findAll('fund'),
       householdMembers: this.store.findAll('household-member'),
       subcategories: this.store.findAll('subcategory'),
       vendors: this.store.findAll('vendor'),

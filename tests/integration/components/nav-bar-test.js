f0bd1c0e7d5ff21aa2e11b1c54dc906d45dc2dd1
Gabriel Tan-Chen <gabriel@tan-chen.com>
2021-09-16 23:38:59 -0400

    Add dropdown

--- tests/integration/components/nav-bar-test.js
+++ tests/integration/components/nav-bar-test.js
@@ -48,13 +48,18 @@ module('Integration | Component | nav-bar', function(hooks) {
     await render(hbs`<NavBar />`);
 
     assert.dom('.nav-links').exists();
-    assert.dom('.nav-links a').exists({ count: 5 });
-    assert.dom('.nav-links a:nth-of-type(1)').containsText('Categories');
-    assert.dom('.nav-links a:nth-of-type(2)').containsText('Income');
-    assert.dom('.nav-links a:nth-of-type(3)').containsText('Funds');
-    assert.dom('.nav-links a:nth-of-type(4)').containsText('Vendors');
-    assert.dom('.nav-links a:nth-of-type(5)').containsText('Members');
-    assert.dom('.nav-links button').exists({ count: 1 });
-    assert.dom('.nav-links button').containsText('Logout');
+    assert.dom('.nav-links > a').exists({ count: 5 });
+    assert.dom('.nav-links > a:nth-of-type(1)').containsText('Categories');
+    assert.dom('.nav-links > a:nth-of-type(2)').containsText('Income');
+    assert.dom('.nav-links > a:nth-of-type(3)').containsText('Funds');
+    assert.dom('.nav-links > a:nth-of-type(4)').containsText('Vendors');
+    assert.dom('.nav-links > a:nth-of-type(5)').containsText('Members');
+    assert.dom('.nav-links > .nav-dropdown').exists();
+    assert.dom('.nav-links > .nav-dropdown > button').exists({ count: 1 });
+    assert.dom('.nav-links > .nav-dropdown > button').hasClass('nav-dropdown-button');
+    assert.dom('.nav-links > .nav-dropdown > button').containsText('User');
+    assert.dom('.nav-links > .nav-dropdown > .nav-dropdown-content').exists();
+    assert.dom('.nav-links > .nav-dropdown > .nav-dropdown-content > button').exists({ count: 1 });
+    assert.dom('.nav-links > .nav-dropdown > .nav-dropdown-content > button:nth-of-type(1)').containsText('Logout');
   });
 });

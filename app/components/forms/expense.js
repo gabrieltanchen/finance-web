cc70d1b80d2cdd7e4ad2828f0cb39e5566a250ad
Gabriel Tan-Chen <gabriel@tan-chen.com>
2022-09-17 18:21:59 -0400

    fixes

--- app/components/forms/expense.js
+++ app/components/forms/expense.js
@@ -1,20 +1,11 @@
-import { A } from '@ember/array';
 import { action, set } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import ENV from 'finance-web/config/environment';
 
 export default class FormsExpenseComponent extends Component {
-  @service session;
   @service store;
   @tracked errors = [];
-  attachments = A([]);
-
-  @action
-  attachmentSelected(index, file) {
-    this.attachments[index].file = file;
-  }
 
   @action
   selectFund(fund) {
@@ -70,39 +61,10 @@ export default class FormsExpenseComponent extends Component {
   }
 
   @action
-  addAttachment() {
-    const newAttachment = this.store.createRecord('attachment', {
-      expense: this.args.expense,
-    });
-    this.attachments.pushObject({
-      attachment: newAttachment,
-      file: null,
-    });
-  }
-
-  @action
   async save(e) {
     e.preventDefault();
     try {
       await this.args.expense.save();
-      for (const attachment of this.attachments) {
-        await attachment.attachment.save();
-        await new Promise((resolve, reject) => {
-          const xhr = new XMLHttpRequest();
-          const fd = new FormData();
-          xhr.open('POST', `${ENV.apiURL}/attachments/${attachment.attachment.id}/upload`);
-          xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 204) {
-              resolve();
-            } else if (xhr.readyState === 4) {
-              reject(new Error(xhr.responseText));
-            }
-          };
-          xhr.setRequestHeader('Authorization', `Bearer ${this.session.authToken}`);
-          fd.append('file', attachment.file);
-          xhr.send(fd);
-        });
-      }
       this.args.saveSuccessful();
     } catch (err) {
       let errors = ['An error occurred. Please try again later.'];

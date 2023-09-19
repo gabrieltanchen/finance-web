3e716f802a39e65db235347eeb775d8156f9afc5
Gabriel Tan-Chen <gabriel@tan-chen.com>
2023-09-18 21:04:31 -0400

    Test helper

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,12 +1,18 @@
 import Application from 'finance-web/app';
 import config from 'finance-web/config/environment';
-import * as QUnit from 'qunit';
+import QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+import { forceModulesToBeLoaded, sendCoverage } from 'ember-cli-code-coverage/test-support';
 
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
+
+QUnit.done(async function() {
+  forceModulesToBeLoaded();
+  await sendCoverage();
+})
 
 start();
